a81695b014e36143ef58a0319726444f2444127d
Jostein Solem <webmaster@miljodir.no>
2018-03-20 11:34:57 +0100

    endret server.js. Feil deklarasjon

--- server.js
+++ server.js
@@ -21,18 +21,6 @@ var serverPort = settings.server_port;
 var domainName = settings.server_domain;
 
 
-const https = require('https');
-const fs = require('fs');
-
-const options = {
-  key: fs.readFileSync('test/fixtures/keys/agent2-key.pem'),
-  cert: fs.readFileSync('test/fixtures/keys/agent2-cert.pem')
-};
-
-https.createServer(options, (req, res) => {
-  res.writeHead(200);
-  res.end('hello world\n');
-}).listen(8000);
 
 
 var WebSocketServer = require('wss').Server
